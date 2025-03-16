c69d0e1d7e476b7fa23382895004f0c4461d7d04
Elis Eriksson <Spelis@duck.com>
2025-03-16 18:03:22 +0100

    rewrite (also uhh, no website currently)

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<link rel="stylesheet" href="/src/assets/app.css" />
+
+<template>
+  <header class="text-3xl">hey there, it appears there is no website here... sorry?</header>
+</template>
